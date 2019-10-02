9bdd55d089cf451c340daf3c93537320e2bf5833
Hahn <hahn.son@optimationgroup.com>
2019-10-02 16:04:38 +1300

    mysql to postsql statement

--- common/setup.js
+++ common/setup.js
@@ -1,11 +1,17 @@
 const fs = require('fs-extra');
 const path = require("path");
-const mysql = require("mysql2/promise");
-const uuidv1 = require("uuid/v1");
+// const mysql = require("mysql2/promise");
+const uuidv1 = require("uuid/v1"); //git repo no longer maintained
 
 // Charset must be be utf8mb4 in db and connection
 // Edit my.cnf on any new mysql sever https://mathiasbynens.be/notes/mysql-utf8mb4
 // Returns a connection promise
+
+const options = {
+
+}
+const pgPromise = require("pg-promise")(options);
+
 module.exports = async (env, resumeSessionId) => {
 
 	const rootDir = path.resolve(__dirname + "/../");
@@ -27,30 +33,51 @@ module.exports = async (env, resumeSessionId) => {
 	// Ensure log directory exists
 	await fs.ensureDir(logDir);
 
-	let connection = await mysql.createConnection({
+	//TODO: connectionString
+	let connection = {
 		host: process.env.DB_HOST,
+		port: "",
 		user: process.env.DB_USER,
 		password: process.env.DB_PASS,
 		database: "cases",
-		charset: "UTF8MB4_UNICODE_CI",
-		multipleStatements: true
-	});
+		client_encoding = 'UTF8'
+	}
+	// await mysql.createConnection({
+	// 	host: process.env.DB_HOST,
+	// 	user: process.env.DB_USER,
+	// 	password: process.env.DB_PASS,
+	// 	database: "cases",
+	// 	charset: "UTF8MB4_UNICODE_CI",
+	// 	multipleStatements: true
+	// });
 
-	let pipeline_connection = await mysql.createConnection({
+	let pipeline_connection = {
 		host: process.env.DB_HOST,
 		user: process.env.DB_USER,
 		password: process.env.DB_PASS,
 		database: "pipeline_cases",
-		charset: "UTF8MB4_UNICODE_CI",
-		multipleStatements: true
-	});
+		port: "",
+		client_encoding = 'UTF8'
+	}
+	
+	// await mysql.createConnection({
+	// 	host: process.env.DB_HOST,
+	// 	user: process.env.DB_USER,
+	// 	password: process.env.DB_PASS,
+	// 	database: "pipeline_cases",
+	// 	charset: "UTF8MB4_UNICODE_CI",
+	// 	multipleStatements: true
+	// });
+
+	const db = await pgPromise(connection);
+	const pipeline_db = await pgPromise(pipeline_connection);
 
 	return {
 		sessionId,
 		cacheDir,
 		logDir,
-		connection,
-		pipeline_connection
+		db,
+		pipeline_db
 	}
 
 };
