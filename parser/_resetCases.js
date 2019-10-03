0ecf5644af27eb69cb8847a68409921ec61fad2c
Hahn <hahn.son@optimationgroup.com>
2019-10-04 10:56:17 +1300

    clean

--- parser/_resetCases.js
+++ parser/_resetCases.js
@@ -6,22 +6,7 @@ const run = async (connection, pipeline_connection, logDir) => {
 	return new Promise(resolve => {
 		setTimeout(async () => {
 			console.log('TRUNCATE "cases" tables');
-			// SET session_replication_role = 'replica';
-			// SET session_replication_role = 'origin';
-
 			//if exists?
-			await connection.query(`
-				SET FOREIGN_KEY_CHECKS = 0 
-				TRUNCATE TABLE case_citations
-				TRUNCATE TABLE case_pdfs
-				TRUNCATE TABLE court_to_cases
-				TRUNCATE TABLE courts
-				TRUNCATE TABLE cases
-				TRUNCATE TABLE cases_cited
-				TRUNCATE TABLE legislation_to_cases 
-				SET FOREIGN_KEY_CHECKS = 1
-			`);
-
 			// ALTER TABLE table_name DISABLE TRIGGER ALL;
 			// TRUNCATE TABLE case_citations;
 			// ALTER TABLE table_name EABLE TRIGGER ALL;
@@ -33,17 +18,15 @@ const run = async (connection, pipeline_connection, logDir) => {
 			// Which I assume I can alter the field to be deferrable first and then using SET CONSTRAINTS ALL DEFERRED; to turn off the foreign key constraint check when truncate the table
 			await connection
 				.tx(t => {
-					return t.batch([
-						t.none(`SET session_replication_role to replica`),
-						t.none(`TRUNCATE TABLE case_citations`),
-						t.none(`TRUNCATE TABLE case_pdfs`),
-						t.none(`TRUNCATE TABLE court_to_cases`),
-						t.none(`TRUNCATE TABLE courts`),
-						t.none(`TRUNCATE TABLE cases`),
-						t.none(`TRUNCATE TABLE cases_cited`),
-						t.none(`TRUNCATE TABLE legislation_to_cases`),
-						t.none(`SET session_replication_role to default`)
-					]);
+					const q1 = t.none("SET session_replication_role to replica");
+					const q2 = t.none("TRUNCATE TABLE case_citations");
+					const q3 = t.none("TRUNCATE TABLE case_pdfs");
+					const q4 = t.none("TRUNCATE TABLE courts");
+					const q5 = t.none("TRUNCATE TABLE cases");
+					const q6 = t.none("TRUNCATE TABLE cases_cited");
+					const q7 = t.none("TRUNCATE TABLE legislation_to_cases");
+					const q8 = t.none("SET session_replication_role to default");
+					return t.batch([q1, q2, q3, q4, q5, q6, q7, q8]);
 				})
 				.then(result => {
 					if (result != null) console.log(result);
@@ -57,24 +40,23 @@ const run = async (connection, pipeline_connection, logDir) => {
 			await pipeline_connection
 				.tx(t => {
 					const count = 0;
-					return t.batch([
-						t.result(
-							`INSERT INTO cases.case_pdfs SELECT * FROM pipeline_cases.case_pdfs`,
-							r => (count += r.rowCount)
-						),
-						t.none(
-							`INSERT INTO cases.cases (id, pdf_id, case_date, case_text, case_name, case_footnotes, case_footnote_contexts) SELECT id, pdf_id, case_date, case_text, case_name, case_footnotes, case_footnote_contexts FROM pipeline_cases.cases`,
-							r => (count += r.rowCount)
-						),
-						t.none(
-							`INSERT INTO cases.case_citations SELECT * FROM pipeline_cases.case_citations`,
-							r => (count += r.rowCount)
-						),
-						t.none(
-							`INSERT INTO cases.legislation SELECT * FROM pipeline_cases.legislation`,
-							r => (count += r.rowCount)
-						)
-					]);
+					const q1 = t.result(
+						"INSERT INTO cases.case_pdfs SELECT * FROM pipeline_cases.case_pdfs",
+						r => (count += r.rowCount)
+					);
+					const q2 = t.none(
+						"INSERT INTO cases.cases (id, pdf_id, case_date, case_text, case_name, case_footnotes, case_footnote_contexts) SELECT id, pdf_id, case_date, case_text, case_name, case_footnotes, case_footnote_contexts FROM pipeline_cases.cases",
+						r => (count += r.rowCount)
+					);
+					const q3 = t.none(
+						"INSERT INTO cases.case_citations SELECT * FROM pipeline_cases.case_citations",
+						r => (count += r.rowCount)
+					);
+					const q4 = t.none(
+						"INSERT INTO cases.legislation SELECT * FROM pipeline_cases.legislation",
+						r => (count += r.rowCount)
+					);
+					return t.batch([q1, q2, q3, q4]);
 				})
 				.then(result => {
 					if (result != null) console.log("count: " + count);
